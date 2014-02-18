56b3fa506081810e5ca08e91463fcdd58d663f91
fiddyspence <chris.spence@puppetlabs.com>
2014-02-18 16:17:53 +0000

    indenting and syntax for class parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,59 +11,51 @@
 # Sample Usage:
 #
 class gerrit (
-  $gerrit_version       = gerrit::params::gerrit_version,
-  $group         		= gerrit::params::gerrit_group,
-  $user          		= gerrit::params::gerrit_user,
-  $gerrit_groups        = gerrit::params::gerrit_groups,
-  $home                 = gerrit::params::gerrit_home,
-  $gerrit_site_name     = gerrit::params::gerrit_site_name,
-  $gerrit_database_type = gerrit::params::gerrit_database_type,
-  $canonical_web_url    = gerrit::params::canonical_web_url,
-  $sshd_listen_address  = gerrit::params::sshd_listen_address,
-  $httpd_listen_url     = gerrit::params::httpd_listen_url,
+  $gerrit_version       = $gerrit::params::gerrit_version,
+  $group                = $gerrit::params::gerrit_group,
+  $user          	= $gerrit::params::gerrit_user,
+  $gerrit_groups        = $gerrit::params::gerrit_groups,
+  $home                 = $gerrit::params::gerrit_home,
+  $gerrit_site_name     = $gerrit::params::gerrit_site_name,
+  $gerrit_database_type = $gerrit::params::gerrit_database_type,
+  $canonical_web_url    = $gerrit::params::canonical_web_url,
+  $sshd_listen_address  = $gerrit::params::sshd_listen_address,
+  $httpd_listen_url     = $gerrit::params::httpd_listen_url,
   $download_mirror      = 'http://gerrit.googlecode.com/files',
-  $stage_dir            = gerrit::params::stage_dir,
-  $war_file_url			= 'http://gerrit-releases.storage.googleapis.com/',
+  $stage_dir            = $gerrit::params::stage_dir,
+  $war_file_url		= 'http://gerrit-releases.storage.googleapis.com/',
   $war_file             = 'gerrit-2.8.1.war',
-)
-  inherits gerrit::params 
-  {
-	  include apache
-	  include java
-	  include tomcat
+) inherits gerrit::params {
+
+  include apache
+  include java
+  include tomcat
+ 
+  package { 'wget':
+    ensure => present,
+  }
+
+  package { 'git':
+    ensure => present,
+  }
 	  
-	  package { 'wget':
-		  ensure => present,
-	  }
-	  
-	  package { 'git':
-		  ensure => present,
-	  }
-	  
-	  user { $gerrit::params::user :
-	    ensure     => present,
-	    home       => $gerrit::params::home, 
-	    managehome => true,
-		before => File[$gerrit::params::home],
-	  }
-	  
-	  file { $gerrit::params::home :
-  		owner => $gerrit::params::user,
-  		group => $gerrit::params::group,
-  		ensure => directory,
-		before => File[$gerrit::params::stage_dir],
-	  }
-	  
-	  # class { 'postgresql::server': }
-	  # 
-	  # postgresql::server::db { 'reviewdb':
-	  #   user     => 'gerrit2',
-	  #   password => postgresql_password('postgres', 'gerrit2'),
-	  # }
-	  
-	  include gerrit::staging
-	  include gerrit::build
-	  include gerrit::install
-  	  include gerrit::start
+  user { $gerrit::params::user :
+    ensure     => present,
+    home       => $gerrit::params::home, 
+    managehome => true,
+    before => File[$gerrit::params::home],
+  }
+
+  file { $gerrit::params::home :
+    owner => $gerrit::params::user,
+    group => $gerrit::params::group,
+    ensure => directory,
+    before => File[$gerrit::params::stage_dir],
+  }
+
+  include gerrit::staging
+  include gerrit::build
+  include gerrit::install
+  include gerrit::start
 
 }
